2095a85bac17d13071a1dd073378f0735726fc78
Boringboys <35089626+Boringboys@users.noreply.github.com>
2023-10-17 15:40:00 +0800

    link egg

--- themes/xoxo-plus/source/js/xoxopegg.js
+++ themes/xoxo-plus/source/js/xoxopegg.js
@@ -1,9 +1,4 @@
 console.log("根据上面字符画tell一个字符串，可以看到一个被隐藏的链接！\n试试用tell\(string\)的方式给我说句话");
 function tell(str){
-    // if(md5(str)=="9e0121b00c93167da9024d1d48a57f6a"){
-    //     givelink(str+md5(str))
-    // }else{
-    //     bibilailai(str)
-    // }
-    givelink2(str);
+    getlink(str);
 }
