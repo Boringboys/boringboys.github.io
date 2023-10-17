89058a5a806a109bd46e9c64ffd3e783600a4e56
Boringboys <boringboys@qq.com>
2023-10-17 15:40:37 +0800

    Site updated: 2023-10-17 15:40:37

--- js/xoxoxhr.js
+++ js/xoxoxhr.js
@@ -33,25 +33,12 @@ function blockXor(arr1, arrkey) {
     return arrret;
 }
 
-function givelink2(key) {
+function getlink(key) {
     let cipherArray = [14, 76, 68, 72, 16, 92, 74, 24, 20, 19, 21, 25, 87, 20, 67, 85, 16, 0, 71, 24, 90, 92, 8, 74];
     plaintext = charCodeArrayToStr(blockXor(cipherArray, strToCharCodeArray(md5(key))));
     if (plaintext.startsWith("https") || plaintext.startsWith("http")) {
-        console.log(plaintext);
+        console.log("链接：\n\t" + plaintext);
     } else {
         bibilailai(key);
     }
 }
-
-function givelink(kkeeyy){
-    if(md5(kkeeyy)=="3ce6f2a4debbc795c71f51bf966875f3"){
-        var xmlhttp=new XMLHttpRequest();
-        xmlhttp.onreadystatechange=function(){
-            if(xmlhttp.readyState==4 && xmlhttp.status==200){
-                console.log("链接：\n"+xmlhttp.response);
-            }
-        };
-        xmlhttp.open("GET","https://text-1254394685.cos.ap-shanghai.myqcloud.com/links/link",true);
-        xmlhttp.send();
-    }
-};
