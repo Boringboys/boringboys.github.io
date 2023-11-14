f6a115a5ae541a410ddc8555c3e2365770eee7ce
Boringboys <boringboys@qq.com>
2023-11-14 16:01:46 +0800

    Site updated: 2023-11-14 16:01:46

--- js/mini_js.js
+++ js/mini_js.js
@@ -175,6 +175,7 @@ function init() {
 			}
 			if(x-lastX > swingSpeed || y-lastY > swingSpeed) {
 				// 向左甩动
+				alert(x-lastX, y-lastY)
 				if (btn.style.display != "none"){
 					clearInterval(timer2);//清除定时器
 					boxMovingFlag = true;
@@ -183,6 +184,7 @@ function init() {
 				}
 			} else if (x-lastX < -swingSpeed || y-lastY < -swingSpeed) {
 				// 向右甩动
+				alert(x-lastX, y-lastY)
 				if (btn.style.display == "none"){
 					clearInterval(timer2);
 					boxMovingFlag = true;
